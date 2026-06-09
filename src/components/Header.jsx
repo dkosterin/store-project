ba5eca25ff7f45ae4dfbad4de9e320e1a65d8405
Python <PyCur@TOP.ru>
2026-06-09 18:52:13 +0200

    focus on search field

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,29 @@
+import { useRef } from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/header.css'
 
 export function Header() {
+  const searchRef = useRef(null);
+
+  function onSearch() {
+    // Если поле поиска пустое, передвинуть в поле курсор
+    // В противном случае "поиск"
+    //console.log(searchRef.current.value);
+    if (searchRef.current.value === "") {
+      searchRef.current.focus();
+    }
+    else {
+      console.log(`Поиск: ${searchRef.current.value}`);
+    }
+  }
+
   return (
     <div className="header">
         <div><Link to="/">Магазин</Link></div>
         <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Поиск" />
-          <button className="search-button">Найти</button>
+          <input className="search-bar" type="text" placeholder="Поиск" 
+            ref={searchRef} />
+          <button onClick={onSearch} className="search-button">Найти</button>
         </div>
         <div className="right-section">
           <div><Link to="orders">Заказы</Link></div>
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,93 +1,9 @@
+import { products } from '../data/products.js'
 import { ProductItem } from './ProductItem.jsx'
 import '../styles/App.css'
 
 export function ProductList() {
-  const products = [
-    {
-      id: 1,
-      title: "Кросовки",
-      seller: "Продавец",
-      price: 1000,
-      image: "./men-athletic-shoes-white.jpg"
-    },
-    {
-      id: 2,
-      title: "Другие кроссовки",
-      seller: "Продавец",
-      price: 600,
-      image: "./men-brown-flat-sneakers.jpg"
-    },
-    {
-      id: 3,
-      title: "Рубашка",
-      seller: "Продавец",
-      price: 1500,
-      image: "./men-golf-polo-t-shirt-gray.jpg"
-    },
-    {
-      id: 4,
-      title: "Рубашка",
-      seller: "Продавец",
-      price: 1500,
-      image: "./electric-steel-hot-water-kettle-white.jpg"
-    },
-    {
-      id: 5,
-      title: "Рубашка",
-      seller: "Продавец",
-      price: 1500,
-      image: "./elegant-white-dinner-plate-set.jpg"
-    },
-    {
-      id: 6,
-      title: "Рубашка",
-      seller: "Продавец",
-      price: 1500,
-      image: "./men-cozy-fleece-hoodie-light-teal.jpg"
-    },
-    {
-      id: 7,
-      title: "Рубашка",
-      seller: "Продавец",
-      price: 1500,
-      image: "./men-navigator-sunglasses-black.jpg"
-    },
-    {
-      id: 8,
-      title: "Рубашка",
-      seller: "Продавец",
-      price: 1500,
-      image: "./women-knit-ballet-flat-white.jpg"
-    },
-    {
-      id: 9,
-      title: "Рубашка",
-      seller: "Продавец",
-      price: 1500,
-      image: "./women-knit-beanie-pom-pom-blue.jpg"
-    },
-    {
-      id: 10,
-      title: "Рубашка",
-      seller: "Продавец",
-      price: 1500,
-      image: "./women-plain-cotton-oversized-sweater-gray.jpg"
-    },
-    {
-      id: 11,
-      title: "Рубашка",
-      seller: "Продавец",
-      price: 1500,
-      image: "./men-stretch-wool-sweater-black.jpg"
-    },
-    {
-      id: 12,
-      title: "Рубашка",
-      seller: "Продавец",
-      price: 1500,
-      image: "./2-slot-toaster-white (1).jpg"
-    },
-  ];
+  
 
   return (
     <div className="products-list">
