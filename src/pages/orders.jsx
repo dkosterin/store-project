cc6736f7c40a43fc712374163336823731e8e677
Python <PyCur@TOP.ru>
2026-08-06 19:30:07 +0200

    fixes

--- src/pages/orders.jsx
+++ src/pages/orders.jsx
@@ -1,5 +1,10 @@
 import "../styles/orders.css";
 
+/*
+1. Получить заказы с сервера
+2. Отобразить эти заказы. Сделать компонент OrderItem({order})
+*/
+
 function OrdersPage() {
     return (
         <>
@@ -11,7 +16,7 @@ function OrdersPage() {
                         <p className="order-date">Заказ от 12.06.2026</p>
                         <p className="order-price">Стоимость: 1000 руб.</p>
                     </div>
-                    <div className="products-list">
+                    <div className="order-products-list">
                         <div className="product-info">
                             <img src="men-athletic-shoes-white.jpg" />
                             <p>Доставка: 12 августа</p>
